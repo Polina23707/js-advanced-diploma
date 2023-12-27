10ba90616d3f7662cca37c3deabfd259144e4a28
Polina Gorelik <Polina23707@gmail.com>
2023-12-27 16:27:10 +0200

    GameState.js first commit

--- src/js/GameState.js
+++ src/js/GameState.js
@@ -1,7 +1,10 @@
+import GameStateService from "./GameStateService";
+
 export default class GameState {
   static from(object) {
     // TODO: create object
-    // console.log(object);
+    let gameStateService = new GameStateService(localStorage);
+    gameStateService.save(object);
     return null;
   }
 }
