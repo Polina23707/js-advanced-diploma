74507198d4501b0dc3c0cc32ac6997c095e1857e
Polina Gorelik <Polina23707@gmail.com>
2023-12-28 14:31:23 +0200

    new commit showTest

--- src/js/__tests__/showTip.test.js
+++ src/js/__tests__/showTip.test.js
@@ -2,25 +2,24 @@
  * @jest-environment jsdom
  */
 
-'use strict';
+import GamePlay from '../GamePlay';
+import GameController from '../GameController';
+import GameState from '../GameState';
 
 jest.mock('../app.js');
 
-import GamePlay from "../GamePlay";
-import GameStateService from "../GameStateService";
-import GameController from "../GameController";
+beforeEach(() => {
+  jest.resetAllMocks();
+});
 
-test('use jsdom in this test file', () => {
-  const gamePlay = new GamePlay();
-  gamePlay.bindToDOM(document.querySelector('#game-container'));
-
-  const stateService = new GameStateService(localStorage);
-
-  const gameCtrl = new GameController(gamePlay, stateService);
-  gameCtrl.init();
-
+test('should ...', () => {
   const element = document.createElement('div');
-  const cell = document.querySelector('.cell');
-  console.log(cell);
-  expect(element).not.toBeNull();
+  element.setAttribute('id', '#game-container');
+  
+  let gamePlay = new GamePlay();
+  gamePlay.bindToDOM(document.querySelector('#game-container'));
+  let gameState = new GameState(localStorage);
+  let gameController = new GameController(gamePlay, gameState);
+  gameController.init();
+  expect(2).toBe(2);
 });
